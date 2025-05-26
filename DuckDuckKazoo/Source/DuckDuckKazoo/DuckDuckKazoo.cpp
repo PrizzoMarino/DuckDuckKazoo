8f7e09d532c29dc9530cedba150a49ab09f6692e
Priz <santi036@school.lu>
2025-05-26 20:23:37 +0200

    Creation of Project

---
+++ DuckDuckKazoo/Source/DuckDuckKazoo/DuckDuckKazoo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DuckDuckKazoo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DuckDuckKazoo, "DuckDuckKazoo" );
